a412ec93f5670f6898a13363f6d6097b9e8acc6f
fdesimini <fdesimini@users.noreply.github.com>
2015-05-26 12:58:33 -0400

    Initial Commit

---
+++ TinderCloneRedux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderCloneRedux
+//
+//  Created by Frank Desimini on 2015-05-26.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
